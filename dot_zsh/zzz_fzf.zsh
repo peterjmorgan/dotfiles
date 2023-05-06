9b52a2a198f2d55e3fb6b48217fd38b74c0c4118
pmorgan <peterjmorgan@gmail.com>
2023-05-06 10:15:13 -0700

    fix zzz_fzf paths

--- dot_zsh/zzz_fzf.zsh
+++ dot_zsh/zzz_fzf.zsh
@@ -1,3 +1,3 @@
-source $location/etc/zsh/fzf_key-bindings.zsh.old
-source $location/etc/zsh/fzf_completion.zsh.old
-source $location/etc/zsh/fzf_options.zsh.old
+source $location/dot_zsh/fzf_key-bindings.zsh.old
+source $location/dot_zsh/fzf_completion.zsh.old
+source $location/dot_zsh/fzf_options.zsh.old
