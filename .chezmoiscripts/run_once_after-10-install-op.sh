394f5ffd99fc54ca3f5d2818a0c3d2ce9ce6cd83
pmorgan <peterjmorgan@gmail.com>
2023-05-09 09:40:09 -0700

    asdf stuff more

--- .chezmoiscripts/run_once_after-10-install-op.sh
+++ .chezmoiscripts/run_once_after-10-install-op.sh
@@ -3,11 +3,11 @@
 set -eufo pipefail
 
 # asdf update >/dev/null 2>&1 || true && echo "= asdf has been updated"
-asdf update || true && echo "= asdf has been updated"
+$HOME/.asdf/bin/asdf update || true && echo "= asdf has been updated"
 
 # asdf plugin-add 1password-cli >/dev/null 2>&1 || true
 # asdf install 1password-cli latest >/dev/null 2>&1 || true && echo "== 1password-cli latest is installed"
 # asdf global 1password-cli latest >/dev/null 2>&1 || true
-asdf plugin-add 1password-cli || true
-asdf install 1password-cli latest || true && echo "== 1password-cli latest is installed"
-asdf global 1password-cli latest || true
+$HOME/.asdf/bin/asdf plugin-add 1password-cli || true
+$HOME/.asdf/bin/asdf install 1password-cli latest || true && echo "== 1password-cli latest is installed"
+$HOME/.asdf/bin/asdf global 1password-cli latest || true
