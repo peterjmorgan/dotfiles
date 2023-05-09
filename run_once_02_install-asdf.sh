368f4344b79127b34b76c085ed06e3f5b1c701d3
pmorgan <peterjmorgan@gmail.com>
2023-05-09 09:44:11 -0700

    more asdf

--- run_once_02_install-asdf.sh
+++ run_once_02_install-asdf.sh
@@ -3,4 +3,5 @@
 if [[ ! $(command -v asdf) ]]; then
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3 >/dev/null 2>&1 || true && echo "~ asdf has been installed"
   echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
+  echo ". $HOME/.asdf/asdf.sh" >> ~/.zshrc
 fi
