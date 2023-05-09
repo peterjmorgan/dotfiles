b7ba8e4fd1e05da08fc79eb98f227d259e03e428
pmorgan <peterjmorgan@gmail.com>
2023-05-09 08:42:12 -0700

    add op install script

---
+++ .chezmoiscripts/run_once_after-10-install-op.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eufo pipefail
+
+asdf update >/dev/null 2>&1 || true && echo "= asdf has been updated"
+
+asdf plugin-add 1password-cli >/dev/null 2>&1 || true
+asdf install 1password-cli latest >/dev/null 2>&1 || true && echo "== 1password-cli latest is installed"
+asdf global 1password-cli latest >/dev/null 2>&1 || true
