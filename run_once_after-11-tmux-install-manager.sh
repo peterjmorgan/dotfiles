4fe3c571b205bcba25e34bfa6d823935262ac876
pmorgan <peterjmorgan@gmail.com>
2023-05-26 21:22:14 -0700

    rework packages-asdf and tmux_install_manager

---
+++ run_once_after-11-tmux-install-manager.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SESSION="install-manager"
+EXIST_SESSION=$(tmux list-sessions | grep $SESSION)
+
+if [ "$EXIST_SESSION" = "" ]
+then
+	# Setup layout
+	tmux new-session -s $SESSION
+	tmux set -g pane-border-status bottom
+	tmux set -g pane-border-format "#{pane_index} #{pane_title}"
+	tmux split-window -t $SESSION:1 -v
+	# tmux split-window -t $SESSION:1 -v
+	tmux select-layout 'even-vertical'
+
+	# pane 1 = tailscaled
+	tmux select-pane -t 1 -T 'asdf packages'
+	tmux select-pane -t 1
+	#PROCESS=$(get_container processor)
+	#tmux send-keys "echo \"process = $PROCESS\""
+	tmux send-keys "bash packages-asdf.sh" C-m
+
+	# pane 2 = tailscale
+	tmux select-pane -t 2 -T 'tailscale'
+	tmux select-pane -t 2
+	#DOWNLOAD=$(get_container downloader)
+	tmux send-keys "just -f $HOME/.config/justfile install-homebrew" C-m
+fi
