602822525324ee820eecb208feff3dc2732ad3c6
pmorgan <peterjmorgan@gmail.com>
2023-05-28 13:02:23 -0700

    comment out packersync again

--- .chezmoiscripts/run_once_after-22-nvim-packersync.sh
+++ .chezmoiscripts/run_once_after-22-nvim-packersync.sh
@@ -4,4 +4,4 @@ if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/" ]; then
 	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
 
-$HOME/.asdf/shims/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+# $HOME/.asdf/shims/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
