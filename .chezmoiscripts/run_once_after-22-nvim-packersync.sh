25e31f9973bcbe4fd2489abf9e640a6dac5aa123
pmorgan <peterjmorgan@gmail.com>
2023-05-28 13:31:57 -0700

    fix stuff

--- .chezmoiscripts/run_once_after-22-nvim-packersync.sh
+++ .chezmoiscripts/run_once_after-22-nvim-packersync.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/" ]; then
-	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-fi
-
 # $HOME/.asdf/shims/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
